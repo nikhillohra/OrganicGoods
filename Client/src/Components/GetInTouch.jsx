a34aed80afdd6acefcaa395a0887b6e07ce86383
nikhillohra <nikhillohra99@gmail.com>
2024-08-13 22:55:46 +0530

    fix

--- Client/src/Components/GetInTouch.jsx
+++ Client/src/Components/GetInTouch.jsx
@@ -4,33 +4,35 @@ const GetInTouch = () => {
   return (
     <section id="getInTouch">
       <div className="flex items-center justify-center p-4 mb-10">
-        <div className="flex flex-col getb  items-center p-6 rounded-lg shadow-md w-full max-w-[70rem]">
-          <h1 className="text-3xl font-semibold text-[rgba(209,143,0,0.96)] mb-4">
+        <div className="flex flex-col items-center p-6 rounded-lg shadow-md w-full max-w-[98%] get getb">
+          <h1 className="text-2xl md:text-3xl font-semibold text-[rgba(209,143,0,0.96)] mb-4">
             Contact Us!
           </h1>
-          <p className="text-lg text-center font-semibold text-[#1b1414] mb-6">
+          <p className="text-base md:text-lg text-center font-semibold text-[#1b1414] mb-6">
             We love hearing from our customers and community members.
           </p>
-          <div className="bg-[#c4d1c434] py-6 p-1.5 m-2 rounded-lg flex items-center justify-center">
-            <div className="space-y-4 text-lg text-[#1b1414]">
-              <div className="flex items-center ">
+          <div className="bg-[#c4d1c434] py-6 px-4 m-2 rounded-lg flex items-center flex-wrap">
+            <div className="space-y-4 text-base md:text-lg text-[#1b1414] ">
+              <div className="flex flex-col sm:flex-row items-start sm:items-center p-1">
                 <span className="font-semibold mr-2">Address:</span>
-                <span>
+                <span className="break-words">
                   Desi Murga and Fish Farm, near Siluwa, Sagda Jhapani, Madhya
                   Pradesh 482056
                 </span>
               </div>
-              <div className="flex items-center">
+              <div className="flex flex-col sm:flex-row items-start sm:items-center">
                 <span className="font-semibold mr-2">Phone:</span>
-                <span>+919098489056 &amp; +917898491294</span>
+                <span className="break-words">
+                  +919098489056 &amp; +917898491294 &amp; +917999635913
+                </span>
               </div>
-              <div className="flex items-center ">
+              <div className="flex flex-col sm:flex-row items-start sm:items-center">
                 <span className="font-semibold mr-2">Email:</span>
-                <span>organicgoodsmp@gmail.com</span>
+                <span className="break-words">organicgoodsmp@gmail.com</span>
               </div>
             </div>
           </div>
-          <h2 className="text-lg text-[#221e1e] font-normal text-center mt-6 mb-4">
+          <h2 className="text-base md:text-lg text-[#221e1e] font-normal text-center mt-6 mb-4">
             *We'll get back to you as soon as possible. Thank you for reaching
             out!
           </h2>
